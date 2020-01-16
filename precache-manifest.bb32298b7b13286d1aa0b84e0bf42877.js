2ec555fd836fbda9f1708e453f5d1e1ec8ad7bec
Tyler Stewart <tyler.stewart1026@gmail.com>
2020-01-15 18:25:06 -0700

    Updates

---
+++ precache-manifest.bb32298b7b13286d1aa0b84e0bf42877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939f7ffb5bbf216bf9ca599abd62ddb5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c3ca2a9bfb04faa9aeea",
+    "url": "/clicky-game/static/css/main.c628b9bc.chunk.css"
+  },
+  {
+    "revision": "741fbe0b2068e4b1dbc6",
+    "url": "/clicky-game/static/js/2.bdd74e25.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.bdd74e25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3ca2a9bfb04faa9aeea",
+    "url": "/clicky-game/static/js/main.2233430e.chunk.js"
+  },
+  {
+    "revision": "060e5f4a8dbf67faf6a1",
+    "url": "/clicky-game/static/js/runtime-main.14fa245e.js"
+  }
+]);
